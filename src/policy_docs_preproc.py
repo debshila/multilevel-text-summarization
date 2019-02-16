01fbe03ab72c5687a166c7b9467d37f7432bbb69
debshila <debshila@gmail.com>
2019-02-15 22:45:45 -0500

    Adding in ACL policy preprocessing and topic modeling

--- src/policy_docs_preproc.py
+++ src/policy_docs_preproc.py
@@ -11,12 +11,7 @@ and lemmatizes the text, and saves the processed file as a pkl.
 """
 # general imports
 import warnings
-import nltk
 import pandas as pd
-import numpy as np
-import scipy as sp
-from time import time, sleep
-import json
 import requests
 import random
 import os
@@ -34,6 +29,7 @@ import html5lib
 import re
 from bs4 import BeautifulSoup
 import requests
+import xml.etree.ElementTree as etree
 
 # Enable logging for gensim - optional
 import logging
@@ -83,6 +79,19 @@ def read_html(file, pat = '[^a-zA-z0-9.?!/ ]+', filt_len=6, doc_type='pp'):
     data = {'file': file, 'year': year, 'doc_type': doc_type, 'text': txt}
     return data
 
+# parse each xml file and generate a df
+def xml_to_df(file):
+#     print(file)
+    e = etree.parse(file)
+    text = [e.findall('.//SUBTEXT')[i].text for i in range(len(e.findall('.//SECTION')))]
+    sec_title = [e.findall('.//SUBTITLE')[i].text for i in range(len(e.findall('.//SECTION')))]
+#    policy_doc = {'file':file, 'text': text, 'title': sec_title}    
+    df = pd.DataFrame({'file':file, 'section':sec_title, 'text': text})
+    return df
+
+
+
+
 # tokenize, lower case, and lemmatize words
 def tokenize(series, stop_words, frequent_words):
     return (
@@ -110,9 +119,10 @@ co_names = co_names_pp + co_names_oo
 # Read in privacy policy and opt-out policy documents and preprocess them
 pp = [read_html(file=file, doc_type='pp') for file in pp_files]
 oo = [read_html(file=file, doc_type='oo') for file in oo_files]
+pp_xml = '/Users/dbm/Downloads/corpus/'
 
 print(
-    f'No. of Privacy policy Documents: {len(pp)} \nNo. of Opt out policy Documents: {len(oo)}'
+    f'No. of Privacy policy Documents: {len(pp)} \nNo. of Opt out policy Documents: {len(oo)}\nNo. of privacy policy XML Documents: {len(pp_xml)}'
 )
 
 # Combine data into dataframe
@@ -139,3 +149,62 @@ print(f'type: {type(data_words)}, len: {len(data_words)}')
 
 ## Save dataset
 dump(data_words, open("/Users/dbm/Documents/Insight S19/data/privacy_optout_policy_cleaned.pkl", "wb"))
+
+########################################
+###### Process ACL XML files ###########
+########################################    
+# Process xml privacy files
+pp_xml = '/Users/dbm/Downloads/corpus/'
+pp_xml_files = ls_fullpath(directory=pp_xml)
+pp_xml_files = [i for i in pp_xml_files if '.DS_Store' not in i]
+
+pp_xml_docs = [ ]
+for file in pp_xml_files:
+    parsed_file = xml_to_df(file, doc_type = 'pp')
+    pp_xml_docs.append(parsed_file)
+
+pp_xml_df = pd.concat(pp_xml_docs, axis = 0)    
+pp_xml_df.head(2)
+
+#  Filter out rows with missing text
+print(pp_xml_df.info())
+print(pp_xml_df.isna().sum())
+# pp_xml_docs = pd.concat(pp_xml_df)
+pp_xml_df = pp_xml_df.reset_index()
+pp_xml_df.head(2)
+
+# Filter out rows with missing text
+pp_xml_df = pp_xml_df[pp_xml_df['text'].notnull()]
+dump(pp_xml_df, open("/Users/dbm/Documents/Insight S19/data/acl_privacy_policy.pkl", "wb"))
+
+print(pp_xml_df.shape)
+# Filter out the Policy names
+pp_df_text = pp_xml_df[pp_xml_df['section'].notnull()]
+print(pp_df_text.shape)
+
+# Retain policy id
+pp_df_name = pp_xml_df[pp_xml_df['section'].isnull()]
+print(pp_df_name.shape)
+
+## Save datasets
+dump(pp_df_text, open("/Users/dbm/Documents/Insight S19/data/privacy_policy_acl_text.pkl", "wb"))
+dump(pp_df_name, open("/Users/dbm/Documents/Insight S19/data/privacy_policy_acl_title.pkl", "wb"))
+
+# Add the company names to frequent words
+co_names_acl = pp_df_text['file'].apply(lambda x: extract_co_name(x)).unique()
+co_names_acl = co_names_acl.tolist()
+frequent_words = frequent_words + co_names_acl
+frequent_words = list(set(frequent_words))
+print(len(frequent_words))
+
+# Clean, tokenize text
+pp_data_words = tokenize(pp_df_text['text'], stop_words=stop_words,
+                      frequent_words=frequent_words)
+
+
+pp_data_words = [' '.join(word) for word in pp_data_words]
+pp_data_words
+print(f'type: {type(pp_data_words)}, len: {len(pp_data_words)}')
+
+dump(pp_data_words, open("/Users/dbm/Documents/Insight S19/data/acl_privacy_policy_words.pkl", "wb"))
+
